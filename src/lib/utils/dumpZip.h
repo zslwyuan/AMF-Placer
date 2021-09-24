14d6f2bcb8aa2e2aab2a95f8abbb9af46fa628c4
tingyuan <tliang@connect.ust.hk>
2021-09-24 20:23:05 +0800

    First pages commit

--- src/lib/utils/dumpZip.h
+++
@@ -1,9 +0,0 @@
-#ifndef _DUMPZIP
-#define _DUMPZIP
-#include <zlib.h>
-#include <string>
-#include <iostream>
-#include <sstream>
-
-void writeStrToGZip(std::string fileName, std::stringstream &data);
-#endif
